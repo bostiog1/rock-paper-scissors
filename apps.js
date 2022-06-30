5b328dbd8167826974e771f5c860df47f31cfa55
Teo <teo.bostiog@yahoo.com>
2022-06-30 22:24:51 +0300

    s

--- apps.js
+++ apps.js
@@ -11,7 +11,24 @@ let userChoice
 possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
 }))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1
+    console.log(randomNumber)
+
+    if(randomNumber === 1) {
+        computerChoice = 'rock'
+    }
+    if(randomNumber === 2) {
+        computerChoice = 'scissors'
+    }
+    if(randomNumber === 3) {
+        computerChoice = 'paper'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+}
 
 
 
