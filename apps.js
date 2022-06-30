9194c3521426767528138f1fe87cdb226c04e605
Teo <teo.bostiog@yahoo.com>
2022-06-30 22:32:45 +0300

    s

--- apps.js
+++ apps.js
@@ -7,11 +7,14 @@ const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 
 let userChoice
+let computerChoice
+let result
 
 possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
     generateComputerChoice()
+    getResult()
 }))
 
 function generateComputerChoice() {
@@ -28,8 +31,32 @@ function generateComputerChoice() {
         computerChoice = 'paper'
     }
     computerChoiceDisplay.innerHTML = computerChoice
+    
 }
 
+function getResult() {
+    if(computerChoice === userChoice) {
+        result = 'it`s a draw!'
+    }
+    if(computerChoice === 'rock' && userChoice === 'paper') {
+        result = 'you won!'
+    }
+    if(computerChoice === 'rock' && userChoice === 'scissors') {
+        result = 'you lost!'
+    }
+    if(computerChoice === 'paper' && userChoice === 'scissors') {
+        result = 'you won!'
+    }
+    if(computerChoice === 'paper' && userChoice === 'rock') {
+        result = 'you lost!'
+    }
+    if(computerChoice === 'scissors' && userChoice === 'rock') {
+        result = 'you won!'
+    }
+    if(computerChoice === 'scissors' && userChoice === 'paper') {
+        result = 'you lost!'
+    }
+}
 
 
 
